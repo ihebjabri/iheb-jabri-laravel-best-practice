f838191fc0a2ac4992733052df860136af8077fb
Iheb Jabri <me.jabri.iheb@gmail.com>
2023-12-20 00:30:07 +0100

    setup docker&nginx

---
+++ project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Start Docker containers
+docker-compose up -d
+
+# Run other commands
+echo "Running other commands..."
+
+# Add more commands here as needed
+
+# The script will not stop Docker containers
